771f2aa5b1d5f36ad5bd680f3dd2510190249292
Alan Sobczak <Asobczak020304@gmial.com>
2022-06-09 00:28:35 +0200

    added Boyer-Moore

--- searchstring/MainWindow.xaml.cs
+++ searchstring/MainWindow.xaml.cs
@@ -148,5 +148,47 @@ namespace searchstring
 				}
 			}
 		}
+		private static int[] BuildBadCharTable2(char[] needle)
+		{
+			int[] badShift = new int[256];
+			for (int i = 0; i < 256; i++)
+			{
+				badShift[i] = needle.Length;
+			}
+			int last = needle.Length - 1;
+			for (int i = 0; i < last; i++)
+			{
+				badShift[(int)needle[i]] = last - i;
+			}
+			return badShift;
+		}
+
+		public static int BoyerMoore(String pattern, String text)
+		{
+			char[] needle = pattern.ToCharArray();
+			char[] haystack = text.ToCharArray();
+
+			if (needle.Length > haystack.Length)
+			{
+				return -1;
+			}
+			int[] badShift = BuildBadCharTable2(needle);
+			int offset = 1;
+			int scan = 0;
+			int last = needle.Length - 1;
+			int maxoffset = haystack.Length - needle.Length;
+			while (offset <= maxoffset)
+			{
+				for (scan = last; (needle[scan] == haystack[scan + offset]); scan--)
+				{
+					if (scan == 0)
+					{
+						return offset;
+					}
+				}
+				offset += badShift[(int)haystack[offset + last]];
+			}
+			return -1;
+		}
 	}
 }
