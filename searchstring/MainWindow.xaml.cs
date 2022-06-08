6ccba8085ef6bdb60e2143b4d5a20f256be1f73e
Alan Sobczak <Asobczak020304@gmial.com>
2022-06-08 23:02:28 +0200

    some changes

--- searchstring/MainWindow.xaml.cs
+++ searchstring/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +27,44 @@ namespace searchstring
         {
             InitializeComponent();
         }
-    }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+			string[] lines = File.ReadAllLines("../../../sonnets.txt");
+			TextBox searchBox = (TextBox)FindName("SearchBox");
+			string SearchPhrase = searchBox.Text;
+
+			if (SearchPhrase != null && SearchPhrase != "")
+			{
+
+				// BruteSearch
+				Stopwatch sw = new Stopwatch();
+				sw.Start();
+				int bruteForce = BruteForce(lines, SearchPhrase);
+				sw.Stop();
+				Label lbl = (Label)FindName("BFlabel");
+				lbl.Content = "Count: " + bruteForce + ", Time (sec): " + sw.Elapsed.TotalSeconds;
+
+
+				// KMP
+				string text = File.ReadAllText("../../../shakespeare-sonnets.txt");
+				Stopwatch sw2 = new Stopwatch();
+				sw2.Start();
+				int[] kmpResults = KMP(text, SearchPhrase);
+				sw2.Stop();
+				Label kmpLbl = (Label)FindName("KMPlabel");
+				kmpLbl.Content = "Count: " + kmpResults.Length + ", Time (sec): " + sw2.Elapsed.TotalSeconds;
+
+
+				// Boyer-Moore
+				Stopwatch sw3 = new Stopwatch();
+				sw3.Start();
+				int bmResult = BoyerMoore(SearchPhrase, text);
+				sw3.Stop();
+				Label bmLbl = (Label)FindName("BMlabel");
+				bmLbl.Content = "Count: " + bmResult + ", Time (sec): " + sw3.Elapsed.TotalSeconds;
+			}
+
+		}
+	}
 }
